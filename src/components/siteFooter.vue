aecf0533a684179561c81986db5f1514071890f8
jong_sun <bluesky0180@naver.com>
2022-03-01 06:50:26 +0900

    side_vue

---
+++ src/components/siteFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-footer color="deep-purple" dark absolute app>
+    <v-spacer />
+    <div>&copy; {{ new Date().getFullYear() + " " + footer }}</div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  props: ["footer"],
+};
+</script>
---
+++ src/components/siteHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-app-bar color="deep-purple" dark app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> Application </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list-item v-for="item in items" :key="item.title" :to="item.to">
+        <v-list-item-content> 
+          {{ item.title }}
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { title: "home", to: "/" },
+        { title: "hello", to: "@/components/HelloWorld" },
+        { title: "about", to: "/about" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
