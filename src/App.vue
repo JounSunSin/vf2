ee6bf4e2d4b96ee66f8235213b6f01987adc38e7
jong_sun <bluesky0180@naver.com>
2022-03-04 17:43:55 +0900

    realtime_database

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,21 @@
     <v-content>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/hello">Hello</router-link>
+      <router-link to="/hello">Hello</router-link>      
       <router-view />
+
+      <div style="height: 300px; text-align: center;">
+        <v-btn icon @click="save">
+            <v-icon>mdi-check</v-icon>
+        </v-btn>&nbsp;&nbsp;
+        <v-btn icon @click="read">
+            <v-icon>mdi-cards-club-outline</v-icon>
+        </v-btn>&nbsp;&nbsp;
+        <v-btn icon @click="readOne">
+			<v-icon>mdi-cards-heart</v-icon>
+        </v-btn>
+      </div>
+
     </v-content>
     <site-footer />
   </div>
@@ -26,5 +39,23 @@ export default {
     siteHeader,
     siteFooter,
   },
+  methods: {
+	save () {
+		console.log('save@@@')
+		this.$firebase.database().ref().child('abcd').set({
+		title: 'abcd', text: 'tttttt'
+		})
+	},
+	read () {
+		this.$firebase.database().ref().child('abcd').on('value', (sn) => {
+		console.log(sn)
+		console.log(sn.val())
+		})
+	},
+	async readOne () {
+		const sn = await this.$firebase.database().ref().child('abcd').once('value')
+		console.log(sn.val())
+	}
+  }
 };
 </script>
